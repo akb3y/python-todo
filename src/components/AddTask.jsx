9e7a628b2d13a49540347ed07306121a891720b8
alisha barraw <alisha.barraw@gmail.com>
2024-04-03 14:00:45 -0600

    created Addtask

---
+++ src/components/AddTask.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const AddTask = () => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const handleTitle = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const handleDescription = (e) => {
+    setDescription(e.target.value);
+  };
+
+  const handleSubmit = async () => {
+    const data = {
+      title: title,
+      description: description
+    };
+    try {
+      const response = await axios.post("http://localhost:3100/todo", data);
+      console.log("Task added successfully!", response.data);
+      // Reset form fields after successful submission
+      setTitle("");
+      setDescription("");
+    } catch (error) {
+      console.error("Error adding task:", error);
+    }
+  };
+
+  return (
+    <form className="form-container" onSubmit={handleSubmit}>
+      <label htmlFor="task">Task Name:</label>
+      <br />
+      <input type="text" id="task" name="task" value={title} onChange={handleTitle} />
+      <br />
+      <label htmlFor="description">Description:</label>
+      <br />
+      <input
+        type="text"
+        id="description"
+        name="description"
+        value={description}
+        onChange={handleDescription}
+      />
+      <br />
+      <input type="submit" value="Add Task" />
+    </form>
+  );
+};
+
+export default AddTask;
