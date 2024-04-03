d3a8639290243a14a8a9a6afbdf1e17c433dd2c7
alisha barraw <alisha.barraw@gmail.com>
2024-04-03 14:00:33 -0600

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import AddTask from "./components/AddTask";
+import Tasks from "./components/Tasks";
+import axios from "axios";
 import "../public/style.css";
 
 const App = () => {
-  return <h1>Hello world! I am using React</h1>;
+  const [data, setData] = useState([]);
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("http://localhost:3100/todo");
+      console.log("Response data:", response.data);
+      setData(response.data.todos);
+    } catch (error) {
+      console.error("Error fetching data:", error.response);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const handleToggleCompleted = async (id) => {
+    try {
+      const updatedData = data.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed: !todo.completed };
+        }
+        return todo;
+      });
+      setData(updatedData);
+
+      // Make a PUT request to update the todo's completed status on the server
+      await axios.patch(`http://localhost:3100/todo/${id}`, {
+        completed: updatedData.find((todo) => todo.id === id).completed
+      });
+    } catch (error) {
+      console.error("Error toggling completed status:", error);
+    }
+  };
+
+  return (
+    <div>
+      <AddTask />
+      <Tasks data={data} handleToggleCompleted={handleToggleCompleted} fetchData={fetchData} />
+    </div>
+  );
 };
 
 export default App;
